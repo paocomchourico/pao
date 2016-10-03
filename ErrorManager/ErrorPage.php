333b9aa3e115a00a309d7c733592b175bfec4c85
fdbatalha <fdbatalha@gmail.com>
2016-10-04 00:15:40 +0100

    Add de codigo à página de erro

--- ErrorManager/ErrorPage.php
+++ ErrorManager/ErrorPage.php
@@ -204,8 +204,6 @@ and open the template in the editor.
     <div class="col-sm-8">
       <h2>EI CA BURRO!!!</h2><br>
       <h4><strong>OLHA SÓ A MERDA QUE FIZESTE:</strong> Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
-      <p><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
   </div>
 </div>
